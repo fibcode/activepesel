900099aac6e8970354f04bd7de550f6b4762bc3d
Karol Musur <kamusur@gmail.com>
2022-04-29 13:17:01 +0200

    fix: use splat operator for options in ActiveModel::Errors#add method

--- lib/active_model/validations/pesel_validator.rb
+++ lib/active_model/validations/pesel_validator.rb
@@ -3,7 +3,7 @@ module ActiveModel
     class PeselValidator < ActiveModel::EachValidator
       def validate_each(record, attr_name, value)
         unless value.blank?
-          record.errors.add(attr_name, :invalid, options) unless Activepesel::Pesel.new(value).valid?
+          record.errors.add(attr_name, :invalid, **options) unless Activepesel::Pesel.new(value).valid?
         end
       end
     end
